6d4ecbe0466449005054703824723c32507e99ae
Will Garrett <the+dev@willgarrett.io>
2018-12-10 18:46:48 -0800

    Still no luck

--- flask-app/app/__init__.py
+++ flask-app/app/__init__.py
@@ -1,8 +1,6 @@
 from flask import Flask
-from redis import Redis
 
 app = Flask(__name__)
-redis = Redis(host='redis', port=6379)
 
 from app import routes
 
--- flask-app/app/routes.py
+++ flask-app/app/routes.py
@@ -1,9 +1,15 @@
 from app import app
+from redis import Redis
+
+
+redis = Redis(host='redis', port=6379)
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
-
+    redis.incr('hits')
+    return "Hello, World! for the %s time" % redis.get('hits')
+'''
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
+'''
